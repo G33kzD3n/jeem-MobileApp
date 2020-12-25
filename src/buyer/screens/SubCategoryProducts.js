a884287a109b9b4048e2ad286f7067d880175c4c
basitmir <iammirbasit@gmail.com>
2020-12-25 20:33:19 +0530

    Remove products from store when page is inactive

--- src/buyer/screens/SubCategoryProducts.js
+++ src/buyer/screens/SubCategoryProducts.js
@@ -4,161 +4,11 @@ import ProductCard from '../../common/components/ProductCard';
 import colors from '../../config/colors';
 import { useSelector, useDispatch } from 'react-redux';
 import { productsAction } from '../../../store/actions';
-import { GET_PRODUCTS_FOR_BUYER } from '../../../store/actions/actionTypes';
+import {
+	GET_PRODUCTS_FOR_BUYER,
+	REMOVE_PRODUCTS_FROM_STORE,
+} from '../../../store/actions/actionTypes';
 import Loader from '../../common/components/Loader';
-
-const data = [
-	{
-		image: [
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-		],
-		title: 'DressBerry',
-		subTitle: 'Colors for only you',
-		details:
-			'Best Product in the market to buy and amazing deals only for you. You will not regret it. Best in the market only for you. Best Product in the market to buy and amazing deals only for you. You will not regret it.',
-		price: 679,
-		orginalPrice: 1049,
-		discount: 60,
-		seller: 'Jeem Solutions',
-	},
-	{
-		image: [
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-		],
-		title: 'DressBerry',
-		subTitle: 'Colors for only you',
-		price: 679,
-		details:
-			'Best Product in the market to buy and amazing deals only for you. You will not regret it. Best in the market only for you. Best Product in the market to buy and amazing deals only for you. You will not regret it.',
-		orginalPrice: 1049,
-		discount: 60,
-		seller: 'Jeem Solutions',
-	},
-	{
-		image: [
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-		],
-		title: 'DressBerry',
-		subTitle: 'Colors for only you',
-		price: 679,
-		details:
-			'Best Product in the market to buy and amazing deals only for you. You will not regret it. Best in the market only for you. Best Product in the market to buy and amazing deals only for you. You will not regret it.',
-		orginalPrice: 1049,
-		discount: 60,
-		seller: 'Jeem Solutions',
-	},
-	{
-		image: [
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-		],
-		title: 'DressBerry',
-		subTitle: 'Colors for only you',
-		price: 679,
-		details:
-			'Best Product in the market to buy and amazing deals only for you. You will not regret it. Best in the market only for you. Best Product in the market to buy and amazing deals only for you. You will not regret it.',
-		orginalPrice: 1049,
-		discount: 60,
-		seller: 'Jeem Solutions',
-	},
-	{
-		image: [
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-		],
-		title: 'DressBerry',
-		subTitle: 'Colors for only you',
-		price: 679,
-		details:
-			'Best Product in the market to buy and amazing deals only for you. You will not regret it. Best in the market only for you. Best Product in the market to buy and amazing deals only for you. You will not regret it.',
-		orginalPrice: 1049,
-		discount: 60,
-		seller: 'Jeem Solutions',
-	},
-	{
-		image: [
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-		],
-		title: 'DressBerry',
-		subTitle: 'Colors for only you',
-		price: 679,
-		details:
-			'Best Product in the market to buy and amazing deals only for you. You will not regret it. Best in the market only for you. Best Product in the market to buy and amazing deals only for you. You will not regret it.',
-		orginalPrice: 1049,
-		discount: 60,
-		seller: 'Jeem Solutions',
-	},
-	{
-		image: [
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-		],
-		title: 'DressBerry',
-		subTitle: 'Colors for only you',
-		price: 679,
-		details:
-			'Best Product in the market to buy and amazing deals only for you. You will not regret it. Best in the market only for you. Best Product in the market to buy and amazing deals only for you. You will not regret it.',
-		orginalPrice: 1049,
-		discount: 60,
-		seller: 'Jeem Solutions',
-	},
-	{
-		image: [
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-		],
-		title: 'DressBerry',
-		subTitle: 'Colors for only you',
-		price: 679,
-		details:
-			'Best Product in the market to buy and amazing deals only for you. You will not regret it. Best in the market only for you. Best Product in the market to buy and amazing deals only for you. You will not regret it.',
-		orginalPrice: 1049,
-		discount: 60,
-		seller: 'Jeem Solutions',
-	},
-	{
-		image: [
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-		],
-		title: 'DressBerry',
-		subTitle: 'Colors for only you',
-		price: 679,
-		details:
-			'Best Product in the market to buy and amazing deals only for you. You will not regret it. Best in the market only for you. Best Product in the market to buy and amazing deals only for you. You will not regret it.',
-		orginalPrice: 1049,
-		discount: 60,
-		seller: 'Jeem Solutions',
-	},
-	{
-		image: [
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-			'https://balinterio.files.wordpress.com/2013/05/cat-tembok.jpg',
-		],
-		title: 'DressBerry',
-		subTitle: 'Colors for only you',
-		price: 679,
-		details:
-			'Best Product in the market to buy and amazing deals only for you. You will not regret it. Best in the market only for you. Best Product in the market to buy and amazing deals only for you. You will not regret it.',
-		orginalPrice: 1049,
-		discount: 60,
-		seller: 'Jeem Solutions',
-	},
-];
 
 const SubCategoryProducts = ({ route }) => {
 	route.params.total = 10632; // set the total items for header
@@ -166,13 +16,13 @@ const SubCategoryProducts = ({ route }) => {
 	// const { subCategoryName } = route.params;
 	const [refreshing, setRefreshing] = useState(false);
 	const productData = useSelector((state) => state.home.products);
-
+	const [loading, setLoading] = useState(false);
 	// console.log(productData, '>>>>>>>>>>>>>>>>>>>>>>>>>', id);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch(productsAction(GET_PRODUCTS_FOR_BUYER, id)); //get called if the user refreshes the page to get data
-		// return () => (productData = []);
+		return () => dispatch({ type: REMOVE_PRODUCTS_FROM_STORE });
 	}, []);
 
 	if (!productData)

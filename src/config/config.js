d79f77f4ab77f86f4096ec8b3b0acc5d752dfb8b
basitmir <iammirbasit@gmail.com>
2021-03-07 22:11:46 +0530

    changing url to https

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,3 @@
-// import axios from "axios";
-
-// const apiUrl = "http://localhost:8000/api/";
-// const apiUrlImage = "http://localhost:8000/storage/images/";
-
-// const config = {
-//   baseURL: apiUrl,
-//   headers: { Authorization: `Bearer ${localStorage.getItem("user_token")}` },
-// };
-// const authAxios = axios.create(config);
-
-// export { apiUrl, apiUrlImage, authAxios };
-
 import axios from 'axios';
 import persistStore from '../utils/persistStore';
 
@@ -19,15 +6,15 @@ import persistStore from '../utils/persistStore';
 // const apiUrlImageStatic = 'http://localhost:8000/storage/images/static/';
 // const apiUrlImageProducts = 'http://localhost:8000/storage/images/products/';
 
-const apiUrl = 'http://jeem-backend.brorinfotech.com/api/';
+const apiUrl = 'https://jeem-backend.brorinfotech.com/api/';
 const apiUrlImage =
-	'http://jeem-backend.brorinfotech.com/public/storage/images/';
+	'https://jeem-backend.brorinfotech.com/public/storage/images/';
 const apiUrlImageStatic =
-	'http://jeem-backend.brorinfotech.com/public/storage/images/static/';
+	'https://jeem-backend.brorinfotech.com/public/storage/images/static/';
 const apiUrlImageProducts =
-	'http://jeem-backend.brorinfotech.com/public/storage/images/products/';
+	'https://jeem-backend.brorinfotech.com/public/storage/images/products/';
 const apiUrlImageCarousels =
-	'http://jeem-backend.brorinfotech.com/public/storage/images/carousels/';
+	'https://jeem-backend.brorinfotech.com/public/storage/images/carousels/';
 
 const config = {
 	baseURL: apiUrl
